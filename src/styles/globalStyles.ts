b58ed4cf4db70d8a38cbc9b82f71def792348ed0
alexander <alexanderhardeland@gmail.com>
2023-07-12 16:17:19 +0200

    Adds css defaults to GlobalStyles that removes any  border, margin and padding defaults

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -1,6 +1,11 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
+  * {
+    border: 0;
+    margin: 0;
+    padding: 0;
+  }
 `;
 
 export default GlobalStyles;
