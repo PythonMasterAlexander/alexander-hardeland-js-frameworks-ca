0b48c56165768c0eabe4e929863f2cff71426e7e
alexander <alexanderhardeland@gmail.com>
2023-07-12 11:47:08 +0200

    Adds last contact page to App main file. Populated the newly created component ContactPage with a return function

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
+import ContactPage from "./pages/ContactPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
           <Route path="products" element={<ProductPage />} />
+          <Route path="contact" element={<ContactPage />} />
         </Route>
       </Routes>
     </>
