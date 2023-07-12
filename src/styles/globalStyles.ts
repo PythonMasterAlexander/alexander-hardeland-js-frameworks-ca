7102515c9d0dade329c56f4b54145710d51178e5
alexander <alexanderhardeland@gmail.com>
2023-07-12 16:36:41 +0200

    Manage to push footer to the bottom of the page in the GlobalStyles file, finished this branch

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -6,6 +6,19 @@ const GlobalStyles = createGlobalStyle`
     margin: 0;
     padding: 0;
   }
+  
+  body, html, #root {
+    height: 100%;
+  }
+
+  main {
+    flex-grow: 1;
+  }
+
+  #root {
+    display: flex;
+    flex-direction: column;
+  }
 `;
 
 export default GlobalStyles;
