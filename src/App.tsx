74855bd033678b1618cd6682284d7809b9b0fbc0
alexander <alexanderhardeland@gmail.com>
2023-07-12 15:43:18 +0200

    Add GlobalStyle to the main App file at the top

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { Routes, Route } from "react-router-dom";
+import GlobalStyles from "./styles/globalStyles";
+
 import Layout from "./components/Layout";
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
@@ -11,6 +13,7 @@ import CheckoutPageSuccess from "./pages/CheckoutPageSuccess";
 function App() {
   return (
     <>
+      <GlobalStyles />
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
