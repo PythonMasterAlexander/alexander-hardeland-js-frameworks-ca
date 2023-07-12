71fb52ed328af5bceddc856ac78fb16e8947787d
alexander <alexanderhardeland@gmail.com>
2023-07-12 11:14:53 +0200

    Adds three Links to the three main pages in the Nav component

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,4 +1,20 @@
+import { Link } from "react-router-dom";
+
 function Nav() {
-  return <nav>Some text</nav>;
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/products">Product</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 export default Nav;
