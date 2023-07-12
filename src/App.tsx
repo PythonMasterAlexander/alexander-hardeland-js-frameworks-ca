91f6fa0a085c959a09c6e9b4bf5a8289f5dcd085
alexander <alexanderhardeland@gmail.com>
2023-07-12 11:39:26 +0200

    Added HomePage and ProductsPage to main App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="products" element={<ProductPage />} />
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
