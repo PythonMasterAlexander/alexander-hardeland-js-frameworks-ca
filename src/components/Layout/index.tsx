388c4bc6307566639d1f4e3938b33dfcd1984245
alexander <alexanderhardeland@gmail.com>
2023-07-12 06:25:38 +0200

    Adds the Footer, Header and Main components to the Layout component

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,4 +1,14 @@
+import Header from "../Header";
+import Footer from "../Footer";
+import Main from "../Main";
+
 function Layout() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
 }
 export default Layout;
