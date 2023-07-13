93d4d0437eb7ebaa680d8f1e8915815e958923d4
alexander <alexanderhardeland@gmail.com>
2023-07-13 15:17:12 +0200

    Adds product description as alt-text

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -30,7 +30,11 @@ function ProductCard() {
     <>
       {products.map((product) => (
         <div className="card" key={product.id}>
-          <img className="product-image" src={product.imageUrl} alt="" />
+          <img
+            className="product-image"
+            src={product.imageUrl}
+            alt={product.description}
+          />
           <div className="card-body">
             <h3 className="card-title">{product.title}</h3>
             <p className="card-info">{product.description}</p>
