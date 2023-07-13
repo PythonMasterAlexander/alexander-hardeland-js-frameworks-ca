514aee59fdb47595e2a7b738a4ff92ba9a181425
alexander <alexanderhardeland@gmail.com>
2023-07-13 18:10:10 +0200

    Adds a try and catch block. Created two new states, isloading and iserror which gets checked if is true or false

--- src/components/ProductCard/ApiCallData.ts
+++ src/components/ProductCard/ApiCallData.ts
@@ -11,19 +11,45 @@ function ApiCallData() {
     imageUrl: string;
   }
 
+  // Api call state
   const [products, setProducts] = React.useState<Array<ApiReturnData>>([]);
+
+  // When loading API, use loading state
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+
+  // If an error when loading API, use error state
+  const [isError, setIsError] = React.useState<boolean>(false);
+
   React.useEffect(() => {
     async function getUrlData() {
-      const response = await fetch(url);
-      const json = await response.json();
+      try {
+        // If an error happens before, reset the error state
+        setIsError(false);
 
-      // Reciving data from the url and then assign it to the setProducts state
-      setProducts(json);
+        // When waiting for products to load, set loading state on
+        setIsLoading(true);
+
+        // API call
+        const response = await fetch(url);
+        const json = await response.json();
+
+        // The result from the API call becomes the  products state
+        setProducts(json);
+
+        // When the API result are a success, clear the loading state
+        setIsLoading(false);
+      } catch (error) {
+        // If there is an error, loading state is stopped
+        setIsLoading(false);
+
+        // If an error, the error state is activated
+        setIsError(true);
+      }
     }
     // Call the function getUrlData
     getUrlData();
   }, []);
 
-  return products;
+  return { products, isLoading, isError };
 }
 export default ApiCallData;
--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,25 +1,32 @@
 import ApiCallData from "./ApiCallData";
 
 function ProductCard() {
-  const products = ApiCallData();
+  const { products, isLoading, isError } = ApiCallData();
 
-  return (
-    <>
-      {products.map((product) => (
-        <div className="card" key={product.id}>
-          <img
-            className="product-image"
-            src={product.imageUrl}
-            alt={product.description}
-          />
-          <div className="card-body">
-            <h3 className="card-title">{product.title}</h3>
-            <p className="card-info">{product.description}</p>
-            <button className="btn-primary to-product">Go to product</button>
+  // TODO create components for error and loading
+  if (isLoading) {
+    return <div>Loading</div>;
+  } else if (isError) {
+    return <div>An error</div>;
+  } else {
+    return (
+      <>
+        {products.map((product) => (
+          <div className="card" key={product.id}>
+            <img
+              className="product-image"
+              src={product.imageUrl}
+              alt={product.description}
+            />
+            <div className="card-body">
+              <h3 className="card-title">{product.title}</h3>
+              <p className="card-info">{product.description}</p>
+              <button className="btn-primary to-product">Go to product</button>
+            </div>
           </div>
-        </div>
-      ))}
-    </>
-  );
+        ))}
+      </>
+    );
+  }
 }
 export default ProductCard;
