1ee8d5bf26b2ed5c692b718715df63c0b8e57925
alexander <alexanderhardeland@gmail.com>
2023-07-17 16:53:17 +0200

    Adds a media query with a max viewport of 400px. on the main section on the home page

--- src/pages/HomePage/index.styles.ts
+++ src/pages/HomePage/index.styles.ts
@@ -8,4 +8,11 @@ export const HomePageMainContainer = styled.section`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   grid-gap: 1.5rem;
+
+  @media (max-width: 800px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+  @media (max-width: 400px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
 `;
