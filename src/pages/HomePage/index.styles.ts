44240e35d5b54cd0d82f844bfcfac838dfc98072
alexander <alexanderhardeland@gmail.com>
2023-07-17 07:36:44 +0200

    Removed styling set in the branch style-main-element-with-flexbox. This is done to start from scratch on setting page responsive layout

--- src/pages/HomePage/index.styles.ts
+++ src/pages/HomePage/index.styles.ts
@@ -4,10 +4,4 @@ export const HeadingOne = styled.h1`
   color: red;
 `;
 
-export const HomePageMainContainer = styled.section`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 2%;
-  width: 90%;
-  margin: 50px auto;
-`;
+export const HomePageMainContainer = styled.section``;
