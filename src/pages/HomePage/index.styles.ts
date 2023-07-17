ed56bac11b5cf3315bcbfe5d6f217aa168007e78
alexander <alexanderhardeland@gmail.com>
2023-07-17 07:24:37 +0200

    Revert "Deleted css responsive code sett on style-main-element-with-flexbox branch. Because I want to start over again creating a responsive layout from the top down in this branch"
    This reverts commit 473f98f487f7d04c1d6aa8b5a0940b01604b8afb.
    
    Something strange happend and suddenly a bunch of files was added. I don't know what happend but I need to find out the problem

--- src/pages/HomePage/index.styles.ts
+++ src/pages/HomePage/index.styles.ts
@@ -4,4 +4,10 @@ export const HeadingOne = styled.h1`
   color: red;
 `;
 
-export const HomePageMainContainer = styled.section``;
+export const HomePageMainContainer = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 2%;
+  width: 90%;
+  margin: 50px auto;
+`;
