f172edc6749ab1a65cb0d3dca6e5b9d1c654d0d2
alexander <alexanderhardeland@gmail.com>
2023-07-17 08:53:09 +0200

    Adds flex on footer with gaps and space between. The same code as on the header

--- src/components/Footer/index.styles.ts
+++ src/components/Footer/index.styles.ts
@@ -1,5 +1,9 @@
 import style from "styled-components";
 
 export const Footer = style.footer`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  gap: 36px;
   padding-bottom: 2%;
 `;
