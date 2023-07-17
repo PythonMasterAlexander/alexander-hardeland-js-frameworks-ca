953b69181541a0cfb360bc16e3a0e55463f5d801
alexander <alexanderhardeland@gmail.com>
2023-07-17 13:08:04 +0200

    Adds display grid and grid template columns to auto and one frame. For now use this code until I have done some more styling work on the cards

--- src/pages/HomePage/index.styles.ts
+++ src/pages/HomePage/index.styles.ts
@@ -4,4 +4,7 @@ export const HeadingOne = styled.h1`
   color: red;
 `;
 
-export const HomePageMainContainer = styled.section``;
+export const HomePageMainContainer = styled.section`
+  display: grid;
+  grid-template-columns: auto 1fr;
+`;
