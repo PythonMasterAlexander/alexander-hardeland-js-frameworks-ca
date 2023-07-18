ec14d67cead24af73cf7ba340c5fe663238f97f9
alexander <alexanderhardeland@gmail.com>
2023-07-18 09:00:52 +0200

    Adds dumy HTML and necessary imports for styled-components in the IndividualProduct directory

---
+++ src/components/IndividualProduct/index.tsx
@@ -0,0 +1,15 @@
+function IndividualProduct() {
+  return (
+    <>
+      <div>
+        <img />
+        <div>
+          <h3>Product header</h3>
+          <p>Product Text</p>
+          <button>Add Product to cart</button>
+        </div>
+      </div>
+    </>
+  );
+}
+export default IndividualProduct;
