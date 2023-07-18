1e9f5f2ecfc11816afa1709ade5cecd13d08abaa
alexander <alexanderhardeland@gmail.com>
2023-07-18 16:24:35 +0200

    Added code back to it's own file in IndividualProduct component. Start over again trying to understand dynamic segments

--- src/components/IndividualProduct/GetIndividualProductData.ts
+++ src/components/IndividualProduct/GetIndividualProductData.ts
@@ -1,38 +1,33 @@
 import * as React from "react";
 import { useParams } from "react-router-dom";
 
-function GetIndividualProductData() {
-  // State for each product
-  const [individualProductData, setIndividualProductData] =
-    React.useState(null);
+// State for each product
+const [individualProductData, setIndividualProductData] = React.useState(null);
 
-  // State for loading
-  const [isLoading, setIsLoading] = React.useState(false);
+// State for loading
+const [isLoading, setIsLoading] = React.useState(false);
 
-  // State for error
-  const [isError, setIsError] = React.useState(false);
+// State for error
+const [isError, setIsError] = React.useState(false);
 
-  // id value for each product
-  //const { id }  = useParams();
-  const url: string = `https://api.noroff.dev/api/v1/online-shop`;
+// id value for each product
+const { id } = useParams();
+//const url: string = `https://api.noroff.dev/api/v1/online-shop/`;
 
-  React.useEffect(() => {
-    async function getApiData() {
-      try {
-        setIsError(false);
-        setIsLoading(true);
+React.useEffect(() => {
+  async function getApiData(url: string) {
+    try {
+      setIsError(false);
+      setIsLoading(true);
 
-        const response = await fetch(url);
-        const json = await response.json();
+      const response = await fetch(url);
+      const json = await response.json();
 
-        setIndividualProductData(json);
-        setIsLoading(false);
-      } catch (error) {
-        setIsError(true);
-      }
+      setIndividualProductData(json);
+      setIsLoading(false);
+    } catch (error) {
+      setIsError(true);
     }
-
-    getApiData();
-  }, []);
-}
-export default GetIndividualProductData;
+  }
+  getApiData(`https://api.noroff.dev/api/v1/online-shop`);
+}, [id]);
--- src/components/IndividualProduct/index.tsx
+++ src/components/IndividualProduct/index.tsx
@@ -1,40 +1,4 @@
-import * as React from "react";
-import { useParams } from "react-router-dom";
-
 function IndividualProduct() {
-  // State for each product
-  const [individualProductData, setIndividualProductData] =
-    React.useState(null);
-
-  // State for loading
-  const [isLoading, setIsLoading] = React.useState(false);
-
-  // State for error
-  const [isError, setIsError] = React.useState(false);
-
-  // id value for each product
-  const { id } = useParams();
-  const url: string = `https://api.noroff.dev/api/v1/online-shop`;
-
-  React.useEffect(() => {
-    async function getApiData(url: string) {
-      try {
-        setIsError(false);
-        setIsLoading(true);
-
-        const response = await fetch(url);
-        const json = await response.json();
-
-        setIndividualProductData(json);
-        setIsLoading(false);
-      } catch (error) {
-        setIsError(true);
-      }
-    }
-    getApiData(url);
-  }, [id]);
-
-  console.log(individualProductData);
   return (
     <>
       <div>
