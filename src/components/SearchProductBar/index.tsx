61661197f71866b84104412bc63687656e9c6019
alexander <alexanderhardeland@gmail.com>
2023-07-19 13:40:20 +0200

    Adds the products from the api call to Search product bar component. Created a function and onChange hook on the input element so I can see what the user types. Next is to create a function that can filter search result and compare it to the values in the products array

--- src/components/SearchProductBar/index.tsx
+++ src/components/SearchProductBar/index.tsx
@@ -1,10 +1,22 @@
+import * as React from "react";
 import * as Styles from "./index.styles";
+import ApiCallData from "../ProductCard/ApiCallData";
 
 function SearchProductBar() {
+  const { products, isLoading, isError } = ApiCallData();
+  console.log(products);
+
+  const [inputValue, setInputValue] = React.useState<string>("");
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const userSearchInputValue = event.target.value.trim().toLowerCase();
+
+    console.log(userSearchInputValue);
+  };
+
   return (
     <>
       <section>
-        <Styles.Input />
+        <Styles.Input onChange={handleInputChange} />
         <button>Search</button>
       </section>
     </>
