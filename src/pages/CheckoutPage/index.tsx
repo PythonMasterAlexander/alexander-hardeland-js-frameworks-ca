b97e0f6df61f46054019019e6a53423abee6bac4
alexander <alexanderhardeland@gmail.com>
2023-07-19 17:35:30 +0200

    Adds a link to checkout successful page on the checkout page

--- src/pages/CheckoutPage/index.tsx
+++ src/pages/CheckoutPage/index.tsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 function CheckoutPage() {
   return (
     <>
       <h1>Hello from the Checkout Page</h1>
+      <Link to="/checkout-was-success">Checkout</Link>
     </>
   );
 }
