3eb9a8970456262b8dc1de5b54ccb08f6101ecb6
alexander <alexanderhardeland@gmail.com>
2023-07-19 12:16:18 +0200

    Adds two new spans element which will contain nr of items and the cart text. Adds a link which links to the cart page

--- src/components/CartIcon/index.tsx
+++ src/components/CartIcon/index.tsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
+
 function CartIcon() {
   return (
     <>
-      <section></section>
+      <section>
+        <Link to="/cart">
+          <span>0</span>
+          <i className="fa-solid fa-cart-shopping fa-2xl"></i>
+          <span>Cart</span>
+        </Link>
+      </section>
     </>
   );
 }
