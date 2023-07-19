0a9d2c173c876c32b59c1ee5a4d2de13f74e1471
alexander <alexanderhardeland@gmail.com>
2023-07-19 18:13:55 +0200

    Adds a basic form on the contact page

--- src/pages/ContactPage/index.tsx
+++ src/pages/ContactPage/index.tsx
@@ -1,7 +1,39 @@
 function ContactPage() {
   return (
     <>
-      <h1>Hello from the contact page</h1>
+      <h1>If you will like to tell us anything</h1>
+      <form>
+        <fieldset>
+          <legend>Contact</legend>
+          <div>
+            <label>
+              Full name
+              <input />
+            </label>
+          </div>
+          <div>
+            <label>
+              Subject
+              <textarea></textarea>
+            </label>
+          </div>
+          <div>
+            <label>
+              Email
+              <input />
+            </label>
+          </div>
+          <div>
+            <label>
+              Body
+              <input />
+            </label>
+          </div>
+          <div>
+            <button>Submit</button>
+          </div>
+        </fieldset>
+      </form>
     </>
   );
 }
