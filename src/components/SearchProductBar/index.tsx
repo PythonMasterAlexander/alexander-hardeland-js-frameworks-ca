c6e2f88e99333401f97ab5d01f667fc362a7c93a
alexander <alexanderhardeland@gmail.com>
2023-07-19 12:45:09 +0200

    Adds a section to search bar component. Adds the component on the home page above the main content section. Adds a section element as the outer container on the search bar component

---
+++ src/components/SearchProductBar/index.tsx
@@ -0,0 +1,10 @@
+function SearchProductBar() {
+  return (
+    <>
+      <section>
+        <h1>Hello from the search bar</h1>
+      </section>
+    </>
+  );
+}
+export default SearchProductBar;
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,9 +1,11 @@
 import * as Styles from "./index.styles";
 import ProductCard from "../../components/ProductCard";
+import SearchProductBar from "../../components/SearchProductBar";
 
 function HomePage() {
   return (
     <>
+      <SearchProductBar />
       <Styles.HeadingOne>Hello from the home page</Styles.HeadingOne>
       <Styles.HomePageMainContainer>
         <ProductCard />
