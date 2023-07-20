b4c4fa921a7937c92725c31de7703132ee18758e
alexander <alexanderhardeland@gmail.com>
2023-07-20 08:46:41 +0200

    Issue not solved. Fix on a later point. Keep the branch

--- src/pages/HomePage/HandleInputChange.ts
+++ src/pages/HomePage/HandleInputChange.ts
@@ -1,6 +1,5 @@
 import * as React from "react";
 
-// Filter value will be used as the state to determen the value of the input field
 export const [filterValue, setFilterValue] = React.useState<string>("");
 
 const HandleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
