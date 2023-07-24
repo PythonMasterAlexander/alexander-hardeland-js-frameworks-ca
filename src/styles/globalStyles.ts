b875f64de5e8dd370b55c596360334b5eca95af9
alexander <alexanderhardeland@gmail.com>
2023-07-24 10:45:09 +0200

    Adds a box-sizing with border-box on the global stylesheet

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -3,6 +3,7 @@ import { createGlobalStyle } from "styled-components";
 const GlobalStyles = createGlobalStyle`
   * {
     border: 0;
+    box-sizing: border-box;
     margin: 0;
     padding: 0;
   }
