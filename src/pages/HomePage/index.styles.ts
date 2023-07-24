7d83fb49365a8e1b0c55787d113d801b9f1e7d97
alexander <alexanderhardeland@gmail.com>
2023-07-24 11:16:03 +0200

    Changed number of frames on the section grid container so four cards are displayed on a screen with 1240px and three cards are displayed on smaller

--- src/pages/HomePage/index.styles.ts
+++ src/pages/HomePage/index.styles.ts
@@ -13,17 +13,24 @@ export const Img = styled.img`
   max-width: 100%;
 `;
 
-export const ProductCardContainer = styled.div``;
+export const ProductCardContainer = styled.div`
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+  width: 100%;
+`;
 
 export const HomePageMainContainer = styled.section`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  justify-content: space-between;
+  grid-template-columns: repeat(4, 1fr);
   grid-gap: 1.5rem;
 
+  @media (max-width: 1240px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
   @media (max-width: 800px) {
     grid-template-columns: repeat(2, 1fr);
   }
-  @media (max-width: 400px) {
+  @media (max-width: 500px) {
     grid-template-columns: repeat(1, 1fr);
   }
 `;
