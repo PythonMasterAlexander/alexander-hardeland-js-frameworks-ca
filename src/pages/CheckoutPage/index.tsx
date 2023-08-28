ef4c648c1e6854df24a5b05fb8dc4c3772d5d8c0
alexander <alexanderhardeland@gmail.com>
2023-08-28 13:11:46 +0200

    Moved the code from CartPage to CheckoutPage, because the assignment say that a cart page is not needed

--- src/pages/CheckoutPage/index.tsx
+++ src/pages/CheckoutPage/index.tsx
@@ -1,9 +1,40 @@
+/*
 import { Link } from "react-router-dom";
 
 function CheckoutPage() {
   return (
     <>
       <h1>Hello from the Checkout Page</h1>
+      <Link to="/checkout-was-success">Checkout</Link>
+    </>
+  );
+}
+export default CheckoutPage;
+*/
+
+import ShoppingCart from "../../components/ShoppingCart";
+import * as Styles from "./index.styles";
+import { Link } from "react-router-dom";
+
+import UseCartStore from "../../components/ShoppingCart/UseCartStore";
+
+function CheckoutPage() {
+  const clearAllProductsFromCartStore = UseCartStore(
+    (state) => state.clearAllProductsFromCartStore
+  );
+
+  return (
+    <>
+      <h1>Hello From the Checkout page</h1>
+      <ShoppingCart />
+      <Styles.IndividualLinkButtonContainer>
+        <Styles.ButtonLink to={"/checkout"}>Checkout</Styles.ButtonLink>
+      </Styles.IndividualLinkButtonContainer>
+      <Styles.IndividualLinkButtonContainer>
+        <Styles.ClearCartButton onClick={() => clearAllProductsFromCartStore()}>
+          Clear shopping cart
+        </Styles.ClearCartButton>
+      </Styles.IndividualLinkButtonContainer>
       <Link to="/checkout-was-success">Checkout</Link>
     </>
   );
