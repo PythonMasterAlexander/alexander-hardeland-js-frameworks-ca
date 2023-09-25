08acb3f5189c031ea71ee7a83238ec20a66d085b
alexander <alexanderhardeland@gmail.com>
2023-09-25 12:09:09 +0200

    Removed the Them provider from App.jsx, renamed the themes file to theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import GlobalStyles from "./styles/globalStyles";
 import { Routes, Route } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
-import { globalThemes } from "./styles/constants/themes";
-
 import Layout from "./components/Layout";
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
@@ -14,22 +11,20 @@ import CheckoutPageSuccess from "./pages/CheckoutPageSuccess";
 function App() {
   return (
     <>
-      <ThemeProvider theme={globalThemes}>
-        <GlobalStyles />
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<HomePage />} />
-            <Route path="product/:id" element={<ProductPage />} />
-            <Route path="contact" element={<ContactPage />} />
-            <Route path="checkout" element={<CheckoutPage />} />
-            <Route
-              path="checkout-was-success"
-              element={<CheckoutPageSuccess />}
-            />
-            <Route path="*" element={<NotFoundPage />} />
-          </Route>
-        </Routes>
-      </ThemeProvider>
+      <GlobalStyles />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="product/:id" element={<ProductPage />} />
+          <Route path="contact" element={<ContactPage />} />
+          <Route path="checkout" element={<CheckoutPage />} />
+          <Route
+            path="checkout-was-success"
+            element={<CheckoutPageSuccess />}
+          />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
     </>
   );
 }
