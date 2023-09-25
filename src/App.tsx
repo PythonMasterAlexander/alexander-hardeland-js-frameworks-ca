f2244bb566d81925940244ec656918fc442e121d
alexander <alexanderhardeland@gmail.com>
2023-09-25 09:03:13 +0200

    Adds a new file themes.ts, this file contains global themes to use as styles. Wrappes the main App function with ThemeProvider to get access to themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import { Routes, Route } from "react-router-dom";
 import GlobalStyles from "./styles/globalStyles";
+import { Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { globalThemes } from "./styles/constants/themes";
 
 import Layout from "./components/Layout";
 import HomePage from "./pages/HomePage";
@@ -12,20 +14,22 @@ import CheckoutPageSuccess from "./pages/CheckoutPageSuccess";
 function App() {
   return (
     <>
-      <GlobalStyles />
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route path="product/:id" element={<ProductPage />} />
-          <Route path="contact" element={<ContactPage />} />
-          <Route path="checkout" element={<CheckoutPage />} />
-          <Route
-            path="checkout-was-success"
-            element={<CheckoutPageSuccess />}
-          />
-          <Route path="*" element={<NotFoundPage />} />
-        </Route>
-      </Routes>
+      <ThemeProvider theme={globalThemes}>
+        <GlobalStyles />
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path="product/:id" element={<ProductPage />} />
+            <Route path="contact" element={<ContactPage />} />
+            <Route path="checkout" element={<CheckoutPage />} />
+            <Route
+              path="checkout-was-success"
+              element={<CheckoutPageSuccess />}
+            />
+            <Route path="*" element={<NotFoundPage />} />
+          </Route>
+        </Routes>
+      </ThemeProvider>
     </>
   );
 }
---
+++ src/styles/constants/themes.ts
@@ -0,0 +1,5 @@
+export const globalThemes = {
+  background: {
+    primary: "red",
+  },
+};
