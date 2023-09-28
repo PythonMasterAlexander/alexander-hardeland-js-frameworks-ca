8808e2a5f33b159f37e4d45906abfa1f654127dc
alexander <alexanderhardeland@gmail.com>
2023-09-28 11:46:56 +0200

    Empty commit

--- src/pages/CheckoutPage/index.tsx
+++ src/pages/CheckoutPage/index.tsx
@@ -1,24 +1,26 @@
-import ShoppingCart from "../../components/ShoppingCart";
-import UseCartStore from "../../components/ShoppingCart/UseCartStore";
 import * as React from "react";
 import * as Styles from "./index.styles";
+import OutputShoppingCart from "../../components/OutputShoppingCart";
+import UseCartStore from "../../components/OutputShoppingCart/UseCartStore";
 import { Link } from "react-router-dom";
-
 function CheckoutPage() {
   const clearAllProductsFromCartStore = UseCartStore(
     (state) => state.clearAllProductsFromCartStore
   );
-
   return (
     <React.Fragment>
-      <h1>Hello From the Checkout page</h1>
-      <ShoppingCart />
-      <Styles.IndividualLinkButtonContainer>
-        <Styles.ClearCartButton onClick={() => clearAllProductsFromCartStore()}>
-          Clear shopping cart
-        </Styles.ClearCartButton>
-      </Styles.IndividualLinkButtonContainer>
-      <Link to="/checkout-was-success">Checkout</Link>
+      <Styles.ShoppingCartFlexContainer>
+        <h1>Hello From the Checkout page</h1>
+        <OutputShoppingCart />
+        <Styles.IndividualLinkButtonContainer>
+          <Styles.ClearCartButton
+            onClick={() => clearAllProductsFromCartStore()}
+          >
+            Clear shopping cart
+          </Styles.ClearCartButton>
+        </Styles.IndividualLinkButtonContainer>
+        <Link to="/checkout-was-success">Checkout</Link>
+      </Styles.ShoppingCartFlexContainer>
     </React.Fragment>
   );
 }
