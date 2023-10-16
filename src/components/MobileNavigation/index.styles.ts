0fe0544264d897ad205d8a1a47efe1746611839a
alexander <alexanderhardeland@gmail.com>
2023-10-16 13:13:21 +0200

    Continue working on mobile navigation

--- src/components/MobileNavigation/index.styles.ts
+++ src/components/MobileNavigation/index.styles.ts
@@ -1,7 +1,6 @@
 import style from "styled-components";
 import { hamburgerOpen } from "./toggleHamburgerOnOff";
 export const MobileNavigation = style.nav`
-  display: ${hamburgerOpen ? "inline-block" : "none"};
 `;
 export const HamburgerMenuClickOnIcon = style.input`
   display: none;
